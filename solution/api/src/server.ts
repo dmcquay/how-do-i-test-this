e06be6aa339efe7858e6fbd13f18124d6628943a
Dustin McQuay <dmcquay@gmail.com>
2020-04-27 16:19:43 +0000

    redirect / to /orders to improve experience when gitpod opens in browser

--- solution/api/src/server.ts
+++ solution/api/src/server.ts
@@ -12,47 +12,51 @@ import config from "./config";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req: Request, res: Response) => {
+    res.redirect('/orders')
+})
+
 app.get("/orders", async (req: Request, res: Response) => {
-  const orders = await getAllOrders();
-  res.send(orders);
+    const orders = await getAllOrders();
+    res.send(orders);
 });
 
 app.get("/orders/:orderId", async (req: Request, res: Response) => {
-  const order = await getOrderById(req.params.orderId);
-  res.send(order);
+    const order = await getOrderById(req.params.orderId);
+    res.send(order);
 });
 
 app.post("/orders", async (req: Request, res: Response) => {
-  if (!isValidCreateOrderRequest(req.body)) {
-    res.status(400);
-    res.send("Invalid create order request");
-    return;
-  }
-  const createOrderRequest = req.body as CreateOrderRequest;
+    if (!isValidCreateOrderRequest(req.body)) {
+        res.status(400);
+        res.send("Invalid create order request");
+        return;
+    }
+    const createOrderRequest = req.body as CreateOrderRequest;
 
-  const riskScore = await getOrderRiskScore(createOrderRequest);
+    const riskScore = await getOrderRiskScore(createOrderRequest);
 
-  const order = createOrderRequestToOrderModel(createOrderRequest, riskScore);
-  await createOrder(order);
-  res.status(303);
-  res.set("location", `/orders/${order.id}`);
-  res.end();
+    const order = createOrderRequestToOrderModel(createOrderRequest, riskScore);
+    await createOrder(order);
+    res.status(303);
+    res.set("location", `/orders/${order.id}`);
+    res.end();
 });
 
 app.get("/order-stats", async (req: Request, res: Response) => {
-  const stats = await getAvgOrderAmountByDay();
-  res.send(stats);
+    const stats = await getAvgOrderAmountByDay();
+    res.send(stats);
 });
 
 async function gracefulShutdown(err: Error) {
-  console.error(err);
-  await pool.end();
-  process.exit(1);
+    console.error(err);
+    await pool.end();
+    process.exit(1);
 }
 
 process.on("uncaughtException", gracefulShutdown);
 process.on("unhandledRejection", gracefulShutdown);
 
 app.listen(config.port, () => {
-  console.log(`API Started. Listening on port ${config.port}.`);
+    console.log(`API Started. Listening on port ${config.port}.`);
 });
